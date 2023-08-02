18d3ccc309f1419d994278cc1f7ceb9b58e48bf3
Rodrigo Santos de Sousa <rodrigocbs914@gmail.com>
2023-08-02 00:20:58 -0300

    feat: incluindo código do front end

---
+++ script.js
@@ -0,0 +1,56 @@
+const sideLinks = document.querySelectorAll('.leftAside .sideMenu li a:not(.logout)');
+
+sideLinks.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', () => {
+        sideLinks.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
+
+const menuBar = document.querySelector('.mainContent nav .bx.bx-menu');
+const leftAside = document.querySelector('.leftAside');
+
+menuBar.addEventListener('click', () => {
+    leftAside.classList.toggle('close');
+});
+
+const searchBtn = document.querySelector('.mainContent nav form .input button');
+const searchBtnIcon = document.querySelector('.mainContent nav form .input button .bx');
+const searchForm = document.querySelector('.mainContent nav form');
+
+searchBtn.addEventListener('click', function (e) {
+    if (window.innerWidth < 576) {
+        e.preventDefault;
+        searchForm.classList.toggle('show');
+        if (searchForm.classList.contains('show')) {
+            searchBtnIcon.classList.replace('bx-search', 'bx-x');
+        } else {
+            searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        }
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 768) {
+        leftAside.classList.add('close');
+    } else {
+        leftAside.classList.remove('close');
+    }
+    if (window.innerWidth > 576) {
+        searchBtnIcon.classList.replace('bx-x', 'bx-search');
+        searchForm.classList.remove('show');
+    }
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark');
+    } else {
+        document.body.classList.remove('dark');
+    }
+});
